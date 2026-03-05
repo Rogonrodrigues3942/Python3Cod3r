69aadf678abca66678de6cd0d212e9d8c1368abb
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2026-03-05 19:22:51 -0300

    Insertinf files in brancj master.

--- curso_rapido_python3/controle/outros_exemplos.py
+++ curso_rapido_python3/controle/outros_exemplos.py
@@ -8,5 +8,18 @@ for p in pessoas:
         print(f'{p} é {a}.')
 
 
-#* Exemplo 2
+#* Exemplo 2: pass
+for i in [1, 2, 3 ]:
+    pass
 
+#* Exemplo 3: continue
+for i in range(1, 11):
+    if i % 2:
+        continue
+    print('conitnue --> número = ', i)
+
+#* Exemplo 4: break
+for i in range(1, 11):
+    if i == 5:
+        break
+    print('break --> Número = ', i)
